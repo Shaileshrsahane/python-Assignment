dfd75f7a4cf580a4fe7434b6d26b3d559661a76d
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-28 14:11:06 +0530

    Create Program_6.py

---
+++ Assignment_15/Program_6.py
@@ -0,0 +1,21 @@
+#write a lambda function which accepts a list and return the minimum element
+
+from functools import reduce
+
+Minimum = lambda No1, No2: No1 if (No1 < No2) else No2
+
+def main():
+    List = []
+    print("Enter the total number of elements: ")
+    TotalNo = int(input())
+
+    print("Enter the elements : ")
+    for i in range(TotalNo):
+      List.append(int(input()))
+
+    Min = reduce(Minimum, List)
+
+    print("Minimum number is : ",Min)
+
+if __name__ == "__main__":
+    main()
