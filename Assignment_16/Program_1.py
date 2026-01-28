26560a0f77a57e08e2836e6c9bb6ee3058988a1d
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-28 19:25:21 +0530

    Create Program_1.py

---
+++ Assignment_16/Program_1.py
@@ -0,0 +1,10 @@
+#write a program which contains one function named as fun(). that should display "hello from fun" on the console
+
+def fun():
+  print("Hello from fun")
+
+def main():
+  fun()
+
+if __name__ == "__main__":
+  main()
