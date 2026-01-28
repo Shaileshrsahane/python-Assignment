1eeb83ffebc2a4ada285ea0be9482849b8100bd3
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-28 19:37:32 +0530

    Create Program_4.py

---
+++ Assignment_16/Program_4.py
@@ -0,0 +1,12 @@
+#write a program which contains one function named as Add(). that accepts two number from user and return addition of twp number
+# Input = 10 5
+# Output = 15
+def Display():
+  for i in range(5):
+    print("Marvellous")
+
+def main():
+  Display()
+
+if __name__ == "__main__":
+  main()    
